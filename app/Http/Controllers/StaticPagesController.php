592fdd9d9f9b23d0a8616e4d35d6c118ff77b273
zhouwengang <php1108@126.com>
2017-04-21 23:04:29 +0800

    no message

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -9,5 +9,18 @@ use App\Http\Controllers\Controller;
 
 class StaticPagesController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('static_pages/home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
 }
