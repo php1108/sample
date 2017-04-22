7a36e11471360e5afc1285e1d698c405580cf257
zhou <php1108@126.com>
2017-04-22 06:52:31 +0000

    finish user signup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,11 +6,42 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\Models\User;
 class UserController extends Controller
 {
-    public function created()
+    public function create()
     {
         return view('users.create');
+    }
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.show',compact('user'));
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+        /**
+         * 5.4.4 唯一性验证
+        一般情况下，我们还需要验证用户使用的注册邮箱是否已被它人使用，这时我们可以使用唯一性验证，这里是针对于数据表 users 做验证。
+
+        'email' => 'unique:users'
+         * 5.4.5 密码匹配验证
+        如果我们需要确保用户在输入密码时，保证两次输入的密码一致。这时候则可以使用 confirmed 来进行密码匹配验证。
+
+        'password' => 'confirmed'
+         */
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,19 @@ get('/help', 'StaticPagesController@help')->name('help');
 get('/about', 'StaticPagesController@about')->name('about');
 
 get('signup','UserController@create')->name('signup');
+resource('users','UserController');
+//get('/users', 'UsersController@index')->name('users.index');
+//get('/users/{id}', 'UsersController@show')->name('users.show');
+//get('/users/create', 'UsersController@create')->name('users.create');
+//post('/users', 'UsersController@store')->name('users.store');
+//get('/users/{id}/edit', 'UsersController@edit')->name('users.edit');
+//patch('/users/{id}', 'UsersController@update')->name('users.update');
+//delete('/users/{id}', 'UsersController@destroy')->name('users.destroy');
+//HTTP 请求	URL	动作	作用
+//GET	/users	UsersController@index	显示所有用户列表的页面
+//GET	/users/1	UsersController@show	显示用户个人信息的页面
+//GET	/users/create	UsersController@create	创建用户的页面
+//POST	/users	UsersController@store	创建用户
+//GET	/users/{id}/edit	UsersController@edit	编辑用户个人资料的页面
+//PATCH	/users/{id}	UsersController@update	更新用户
+//DELETE	/users/{id}	UsersController@destroy	删除用户
